52d964d32886fa0e76fcc6603fe3b1c683601249
Staffan Enberg <senberg@live.se>
2020-10-25 13:38:03 +0100

    Added TileSet class.

---
+++ src/main/java/senberg/MapGenerator.java
@@ -0,0 +1,15 @@
+package senberg;
+
+public class MapGenerator {
+    int size;
+
+    public MapGenerator(int size){
+        this.size = size;
+    }
+
+    public boolean[][] generateWater(){
+        boolean[][] original = new boolean[size][size];
+        return original;
+
+    }
+}
